a74f95b18c9c368d6969e637d2b5af93b868aa77
JGenereux <jacegenereux@gmail.com>
2025-02-20 08:20:16 -0700

    Replaced href on buttons on homepage with onClick handlers using useNavigate

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material"
 import Navbar from "./Navbar"
 import earthIcon from '../assets/Images/earth.png'
 import lockIcon from '../assets/Images/secure-lock.png'
+import { useNavigate } from "react-router-dom"
 
 document.body.style.backgroundColor = "#121212"
 
@@ -19,16 +20,18 @@ export default function Home() {
 }
 
 function JoinMatch() {
+    const navigate = useNavigate()
+
     return <div className="flex flex-row w-[65%] md:w-[40%] h-fit justify-center items-center space-x-6">
         <div className="flex flex-col bg-white text-black bg-opacity-90 space-y-1 w-40 h-full rounded-[4px] p-2 items-center text-center text-xs md:text-sm">
             <p className="font-basicFont">Play against anyone</p>
             <img src={earthIcon} className="w-[96%] md:w-[80%]"></img>
-            <Button href="/lobby" sx={{ color: 'black', padding: ['3px', '5px'], fontSize: ['12px'], fontFamily: 'headerFont', border: 'solid black', borderWidth: '1.2px', boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.25)' }}>Join Match</Button>
+            <Button onClick={() => navigate('/lobby')} sx={{ color: 'black', padding: ['3px', '5px'], fontSize: ['12px'], fontFamily: 'headerFont', border: 'solid black', borderWidth: '1.2px', boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.25)' }}>Join Match</Button>
         </div>
         <div className="flex flex-col space-y-1 bg-white text-black bg-opacity-90 w-40 h-full rounded-[4px] p-2 items-center text-center text-xs md:text-sm">
             <p className="font-basicFont">Play against friends</p>
             <img src={lockIcon} className="w-[100%] md:w-[80%]"></img>
-            <Button href="/privateLobby" sx={{ color: 'black', padding: ['3px', '5px'], fontSize: ['12px'], fontFamily: 'headerFont', border: 'solid black', borderWidth: '1.2px', boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.25)' }}>Start Match </Button>
+            <Button onClick={() => navigate('/privateLobby')} sx={{ color: 'black', padding: ['3px', '5px'], fontSize: ['12px'], fontFamily: 'headerFont', border: 'solid black', borderWidth: '1.2px', boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.25)' }}>Start Match </Button>
         </div>
     </div >
 }
