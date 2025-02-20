a67494608af9a97715577cca12c38a47c38a2a94
JGenereux <jacegenereux@gmail.com>
2025-02-20 09:42:50 -0700

    praying

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -4,3 +4,6 @@ const URL = 'https://codeblitz.up.railway.app/'
 
 export const socket = io(URL)
 
+socket.on("connect", () => {
+    console.log("✅ Successfully connected to WebSocket! ID:", socket.id);
+});
