5fc0de720ff87efb9c158467b96ca7698c039b6d
JGenereux <jacegenereux@gmail.com>
2025-02-21 03:08:20 -0700

    Fixed socket issues with connecting

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,7 +1,6 @@
 import {io} from 'socket.io-client'
 
 const URL = 'https://codeblitz.up.railway.app/'
-const testURL = 'http://localhost:5000/'
 
-export const socket = io(testURL)
+export const socket = io(URL)
 
